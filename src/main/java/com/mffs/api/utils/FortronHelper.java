78b2ff9d4c975e2a14b06a2fdfffef891a3af71a
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2017-04-15 11:49:35 +0200

    Rewritten Fortron Capacitor modes according to in-game tooltips.

--- src/main/java/com/mffs/api/utils/FortronHelper.java
+++ src/main/java/com/mffs/api/utils/FortronHelper.java
@@ -49,45 +49,73 @@ public class FortronHelper
 
         switch (mode)
         {
-
             case DISTRIBUTE:
+                //spread energy evenly to all machines (amount-based)
                 for (IFortronFrequency machine : tiles)
                 {
                     transfer(freq, machine, (fortron / tiles.size()) - machine.getFortronEnergy(), limit);
                 }
                 return;
 
-            case EQUALIZE: //TODO: This should be different than drain!?
+            case FILL:
+                //fill the capacitor and EQUALIZE the remaining energy
+
+                if (freq.getFortronEnergy() < freq.getFortronCapacity())
+                {
+                    //drain energy from other machines
+                    for (IFortronFrequency machine : tiles)
+                    {
+                        //It doesn't matter how much energy is actaully transfered,
+                        //it will be equalized when an internal container is full.
+                        transfer(machine, freq, limit, limit);
+                    }
+                    return;
+                }
+
+            case DRAIN:
+                //similar to EQUALIZE but without storing energy in this machine
+
                 //tiles.add(freq);//we wanna equally send to ourselves.
-            case DRAIN://Seems like the same as EQUALIZE
+
+                if (mode.equals(TransferMode.DRAIN) && (freq.getFortronEnergy() > 1000 || tiles.size() < 2))
+                {
+                    //First empty internal container; spread energy just like in DISTRIBUTE mode.
+                    for (IFortronFrequency machine : tiles)
+                    {
+                        //it doesn't matter how much energy is actaully transfered,
+                        //it will be equalized when an internal container is empty.
+                        transfer(freq, machine, limit, limit);
+                    }
+                }
+                else
+                {
+                    //now spread the energy evenly between other machines, same as in EQUALIZE mode.
+                    IFortronFrequency[] machines = new IFortronFrequency[tiles.size()];
+                    tiles.toArray(machines);
+
+                    for (int i = 0; i < machines.length - 1; i++)
+                    {
+                        int num = machines[i + 1].getFortronCapacity() * machines[i].getFortronEnergy()
+                                    - machines[i].getFortronCapacity() * machines[i + 1].getFortronEnergy();
+                        int den = machines[i].getFortronCapacity() + machines[i + 1].getFortronEnergy();
+                        int transfer = (int) Math.floor(num / den + 0.5f);
+
+                        //energy is actually transmitted bewteen two machines without the FortronCapacitor, so a custom
+                        //beams must be drawn.
+                        transfer(machines[i], machines[i + 1], transfer, limit, freq);
+                    }
+                }
+                return;
+
+            case EQUALIZE:
+                //spread energy evenly to all machines (procentage-based)
+
                 //tiles.remove(freq);
                 for (IFortronFrequency machine : tiles)
                 {
                     int transfer = (int) (((double) machine.getFortronCapacity() / capacity) * fortron) - machine.getFortronEnergy();
-                    if (mode == TransferMode.DRAIN && transfer <= 0)
-                    {
-                        continue;
-                    }
                     transfer(freq, machine, transfer, limit);
                 }
-                break;
-
-            case FILL:
-                if (freq.getFortronEnergy() >= freq.getFortronCapacity())
-                {
-                    return;
-                }
-                int transfer = freq.getFortronCapacity() - freq.getFortronEnergy();
-                for (IFortronFrequency machine : tiles)
-                {
-                    int consume = Math.min(transfer, machine.getFortronEnergy());
-                    transfer = -machine.getFortronEnergy() - consume;
-                    if (consume > 0)
-                    {
-                        transfer(freq, machine, transfer - machine.getFortronEnergy(), limit);
-                    }
-                }
-                break;
         }
     }
 
@@ -98,8 +126,9 @@ public class FortronHelper
      * @param rec   The receiving machine.
      * @param joul  The jouls to be sent.
      * @param limit The limit per tick.
+     * @param intermediate optional intermediate machine beam will be drawn through
      */
-    public static void transfer(IFortronFrequency freq, IFortronFrequency rec, int joul, int limit)
+    public static void transfer(IFortronFrequency freq, IFortronFrequency rec, int joul, int limit, IFortronFrequency intermediate)
     {
         TileEntity entity = (TileEntity) freq;
         World world = entity.getWorldObj();
@@ -118,7 +147,28 @@ public class FortronHelper
 
         if (world.isRemote && toBeInject > 0 && !camo)
         {
-            ModularForcefieldSystem.proxy.registerBeamEffect(world, new Vector3D((TileEntity) freq).translate(.5), new Vector3D((TileEntity) rec).translate(.5), 0.6F, 0.6F, 1, 20);
+            if (intermediate == null)
+            {
+                ModularForcefieldSystem.proxy.registerBeamEffect(world, new Vector3D((TileEntity) freq).translate(.5), new Vector3D((TileEntity) rec).translate(.5), 0.6F, 0.6F, 1, 20);
+            }
+            else
+            {
+                //draw beams: freq <--> intermediate and intermediate <--> rec
+                Vector3D interVector = new Vector3D((TileEntity) intermediate).translate(.5);
+                ModularForcefieldSystem.proxy.registerBeamEffect(world, new Vector3D((TileEntity) freq).translate(.5),
+                        interVector,0.6F, 0.6F, 1, 20);
+                ModularForcefieldSystem.proxy.registerBeamEffect(world, interVector,
+                        new Vector3D((TileEntity) rec).translate(.5), 0.6F, 0.6F, 1, 20);
+            }
         }
+    }
+
+    /**
+     * Same as {@link #transfer(IFortronFrequency, IFortronFrequency, int, int, IFortronFrequency)} with last param
+     * set to <i>null</i>.
+     */
+    public static void transfer(IFortronFrequency freq, IFortronFrequency rec, int joul, int limit)
+    {
+        transfer(freq, rec, joul, limit, null);
     }
 }
